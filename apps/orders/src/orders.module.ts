9751d6ae013f6a15db595800c2f1a2c5ffbd549c
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-07-25 13:49:06 +0300

    Connect mongo

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@app/common/database/database.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { OrdersRepository } from './orders.repository';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({ MONGODB_URI: Joi.string().required() }),
+      envFilePath: '/apps/orders/.env',
+    }),
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    DatabaseModule,
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
